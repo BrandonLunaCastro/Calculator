74c4c929a8bd6e339573d3d6224284434d2ce0bd
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-08-03 12:46:11 -0300

    fixed: add functionality to undo button
    BREAKING CHANGE: undo button deletes the last digit present on display calculator

--- js/index.js
+++ js/index.js
@@ -12,19 +12,19 @@ const operate  = (operate,n1,n2) => {
     
     switch (operate) {
         case '+':
-        result = suma(n1,n2)
+        result = suma(n1,n2);
             break;
         case '-':
-        result = resta(n1,n2)
+        result = resta(n1,n2);
             break;
         case '*' :
-        result = multiplicacion(n1,n2)
+        result = multiplicacion(n1,n2);
             break;
         case '/' : 
-            n2 === 0 ? result= 'no se puede dividir por 0' : result = division(n1,n2)
+            n2 === 0 ? result= 'no se puede dividir por 0' : result = division(n1,n2);
             break;
         default:
-        result = 'Math Error'
+        result = 'Math Error';
             break;
     }   
     return result
@@ -33,69 +33,84 @@ const operate  = (operate,n1,n2) => {
 
 let btns = document.querySelectorAll("button"),
     input = document.querySelector(".input"),
-    output = document.querySelector(".output")
+    output = document.querySelector(".output");
     console.log(input.innerText) 
 
 let num1,
     operando,
-    num2
+    num2;
 /* Funcion que recibe los valores que pasara a la funcion operate , esta a su vez
 actualiza el display de la calculadora */
 const pintarDisplay = (valor) =>{ 
     
     if(input.innerText === "0" && !(valor === "clear") && !(valor==='=')){
-        input.innerText = valor
-        num1 = valor
+        input.innerText = valor;
+        num1 = valor;
     }else if(valor === "clear"){
-        input.innerText = "0"
-        output.innerText = " "
-        num1 = "0"
-        operando = ""
-        num2 = ""
+        input.innerText = "0";
+        output.innerText = " ";
+        num1 = "0";
+        operando = "";
+        num2 = "";
+    }else if(valor === '=' && num1 && !operando && !num2){
+        num1 === '0' ? output.innerText = 'Math Error' : output.innerText = num1;
     }else if(valor === "="){
-        input.innerText += ""
+        input.innerText += "";
         output.innerText = operate(operando,Number(num1),Number(num2));
         num1 = operate(operando,Number(num1),Number(num2));
-        num2 = ''
-        operando = ""
+        num2 = '';
+        operando = "";
     }else if((valor === "+" || valor === "-" || valor === "*" || valor === "/") && !operando ){
-        input.innerText += valor
-        operando = valor
+        input.innerText += valor;
+        operando = valor;
     }else if(!operando && !(valor == "undo")){
-        input.innerText += valor
-        num1 += valor
+        input.innerText += valor;
+        num1 += valor;
      
     }else if(operando && (valor === "+" || valor === "-" || valor === "*" || valor === "/" )){
-        num1 = operate(operando,Number(num1),Number(num2)) 
-        output.innerText = num1
-        operando = valor
-        input.innerText += valor
-        num2 = '' 
+        num1 = operate(operando,Number(num1),Number(num2));
+        output.innerText = num1;
+        operando = valor;
+        input.innerText += valor;
+        num2 = '';
     /* Esta condicion analiza si ya existe un operando cargado en la variable y si el nuevo valor que se ingresa es otro operando, 
        entonces realiza la operacion con los numeros dispuestos por el usuario y carga el nuevo operando mostrando en pantalla el
        resultado
     */   
     }else if(valor === 'undo'){
-        let longitud = input.innerText; 
-        if(longitud.length === 1){
-            input.innerText = 0
-        }else{
-            input.innerText = longitud.slice(0,longitud.length-1);  
-            
-        } 
+        let longitud = input.innerText,
+            ultimo = longitud[longitud.length-1],
+            compruebaNum = /\d/.test(ultimo) 
+        
+        longitud.length === 1 
+            ?  input.innerText = 0 
+            : input.innerText = longitud.slice(0,longitud.length-1);
+
+        /*Esta pieza de codigo analiza , si el ultimo digito del display es un numero y si aun no se ha ingresado un operando 
+         quiere decir que el numero presente pertenece al primer numero de la operacion, sino
+         se comprueba que el ultimo sea uno de los operandos disponibles y en caso de ser limpia el operando actual
+         tanto de display como de la variable, por ultimo en caso de no ser un simbolo se actualiza el valor 
+         al segundo numero de la operacion */
+
+        compruebaNum && operando === undefined 
+            ? num1 = num1.slice(0,num1.length-1)
+            : /[+,-,/,*]/.test(ultimo) 
+              ? operando = undefined 
+              : num2 = num2.slice(0,num2.length-1);
+    
+        console.log(num1,operando,num2);
     }else{
-        num2 === undefined ? num2 = valor : num2 += valor
-        input.innerText += valor
-      //output.innerText =  operate(operando,Number(num1),Number(num2)) 
+        num2 === undefined ? num2 = valor : num2 += valor;
+        input.innerText += valor;
     }
     
-    console.log(num1,operando,num2)
+    console.log(num1,operando,num2);
         
 }
 
 btns.forEach(btn => {
     btn.addEventListener("click",e => {
-        let valor = e.target.value
-        pintarDisplay(valor)        
+        let valor = e.target.value;
+        pintarDisplay(valor);   
     })
 });
