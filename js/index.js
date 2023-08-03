26433d2c3c3ca8e3154083b0fdb9462fac0f93e7
Brandon Luna Castro <blunacastro2003@gmail.com>
2023-08-03 20:25:33 -0300

    fixed:add keyboard support and final styles to the page

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 /* Funciones Aritmeticas */
-const suma = (n1,n2) => n1 + n2;
+const isInteger = (num1,num2) =>  Number.isInteger(num1) && Number.isInteger(num2)
 
-const resta = (n1,n2) => n1 - n2; 
+const suma = (n1,n2) => isInteger(n1,n2) ? n1 + n2 : (n1 + n2).toFixed(2) 
 
-const multiplicacion = (n1,n2) =>  n1 * n2; 
+const resta = (n1,n2) => isInteger(n1,n2) ? n1 - n2 : (n1 - n2).toFixed(2); 
 
-const division = (n1,n2) => n1 / n2;
+const multiplicacion = (n1,n2) => isInteger(n1,n2) ? n1 * n2 : (n1 * n2).toFixed(2); 
+
+const division = (n1,n2) => isInteger(n1,n2) ? n1 / n2 :(n1 / n2).toFixed(2);
+
 /* Segun el operando que se recibe esta funcion maneja que operacion aritmetica se ejecuta */
 const operate  = (operate,n1,n2) => {
     let result
@@ -30,7 +33,6 @@ const operate  = (operate,n1,n2) => {
     return result
 }
 
-
 let btns = document.querySelectorAll("button"),
     input = document.querySelector(".input"),
     output = document.querySelector(".output");
@@ -39,30 +41,34 @@ let btns = document.querySelectorAll("button"),
 let num1,
     operando,
     num2;
-/* Funcion que recibe los valores que pasara a la funcion operate , esta a su vez
+/* Este codigo que recibe los valores que pasara a la funcion operate , esta a su vez
 actualiza el display de la calculadora */
 const pintarDisplay = (valor) =>{ 
-    
-    if(input.innerText === "0" && !(valor === "clear") && !(valor==='=')){
+    if(input.innerText === "0" && !(valor === "clear" || valor === "Backspace") && !(valor === "=" || valor === "Enter")){ 
+        console.log("entro")
         input.innerText = valor;
         num1 = valor;
-    }else if(valor === "clear"){
+    }else if(valor === "clear" || valor === "Backspace"){
         input.innerText = "0";
         output.innerText = " ";
         num1 = "0";
         operando = "";
         num2 = "";
-    }else if(valor === '=' && num1 && !operando && !num2){
+        document.querySelector(".item-16").removeAttribute("disabled")
+    }else if((valor === '='|| valor === "Enter" ) && num1 && !operando && !num2){
         num1 === '0' ? output.innerText = 'Math Error' : output.innerText = num1;
-    }else if(valor === "="){
-        input.innerText += "";
+        document.querySelector(".item-16").removeAttribute("disabled")
+    }else if(valor === "=" || valor === "Enter"){
+     
         output.innerText = operate(operando,Number(num1),Number(num2));
         num1 = operate(operando,Number(num1),Number(num2));
         num2 = '';
         operando = "";
+        document.querySelector(".item-16").removeAttribute("disabled")
     }else if((valor === "+" || valor === "-" || valor === "*" || valor === "/") && !operando ){
         input.innerText += valor;
         operando = valor;
+        document.querySelector(".item-16").removeAttribute("disabled")
     }else if(!operando && !(valor == "undo")){
         input.innerText += valor;
         num1 += valor;
@@ -73,15 +79,14 @@ const pintarDisplay = (valor) =>{
         operando = valor;
         input.innerText += valor;
         num2 = '';
-    /* Esta condicion analiza si ya existe un operando cargado en la variable y si el nuevo valor que se ingresa es otro operando, 
-       entonces realiza la operacion con los numeros dispuestos por el usuario y carga el nuevo operando mostrando en pantalla el
+    /* Esta condición analiza si ya existe un operando cargado en la variable y si el nuevo valor que se ingresa es otro operando, 
+       entonces realiza la operación con los numeros dispuestos por el usuario y carga el nuevo operando mostrando en pantalla el
        resultado
     */   
     }else if(valor === 'undo'){
         let longitud = input.innerText,
             ultimo = longitud[longitud.length-1],
-            compruebaNum = /\d/.test(ultimo) 
-        
+            compruebaNum = /\d|\./.test(ultimo) 
         longitud.length === 1 
             ?  input.innerText = 0 
             : input.innerText = longitud.slice(0,longitud.length-1);
@@ -91,6 +96,7 @@ const pintarDisplay = (valor) =>{
          se comprueba que el ultimo sea uno de los operandos disponibles y en caso de ser limpia el operando actual
          tanto de display como de la variable, por ultimo en caso de no ser un simbolo se actualiza el valor 
          al segundo numero de la operacion */
+        ultimo === "." ? document.querySelector(".item-16").removeAttribute("disabled") : "" ;
 
         compruebaNum && operando === undefined 
             ? num1 = num1.slice(0,num1.length-1)
@@ -98,7 +104,7 @@ const pintarDisplay = (valor) =>{
               ? operando = undefined 
               : num2 = num2.slice(0,num2.length-1);
     
-        console.log(num1,operando,num2);
+    //      console.log(num1,operando,num2);
     }else{
         num2 === undefined ? num2 = valor : num2 += valor;
         input.innerText += valor;
@@ -107,10 +113,39 @@ const pintarDisplay = (valor) =>{
     console.log(num1,operando,num2);
         
 }
-
+/*  Esta función comprueba que el valor ingresado sea un punto y en caso de ser asi 
+    verifica que el primer número tenga un punto , si lo tiene desabilita el boton para que no
+    se repita dos veces en el mismo número, en caso de que no lo incluya eso quiere decir que
+    se ha presionado la tecla "undo" la cual borra dígito por dígito de esta manera 
+    habilita nuevamente el botón
+*/
+const compruebaComa = (valor) => {
+    valor !== "."
+     ? "" 
+     : num1.includes(".") 
+       ? document.querySelector(".item-16").setAttribute("disabled","") 
+       : document.querySelector(".item-16").removeAttribute("disabled");
+}
+/* Esta parte captura los eventos de las teclas propias de la calculadora */
 btns.forEach(btn => {
     btn.addEventListener("click",e => {
-        let valor = e.target.value;
+        let valor = e.target.value
+          
         pintarDisplay(valor);   
+        compruebaComa(valor)
     })
 });
+
+/* Esta parte del codigo captura eventos del teclado */
+window.addEventListener("keydown", e  => {
+    e.preventDefault()
+    let numeros = /\d/ig,
+        simbolos = /[+,/,*,.,-]/,
+        access = /(Enter)|(Backspace)/
+         if(numeros.test(e.key) || simbolos.test(e.key) || access.test(e.key)){
+            pintarDisplay(e.key)
+            compruebaComa(e.key)
+         } 
+})
+
+
